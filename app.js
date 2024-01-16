9b32df02209a2222573321eb296f807a95228785
Md sifat <mdsifat7252@gmail.com>
2024-01-16 20:51:15 +0600

    bug fixed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.querySelector(".btn2").disabled = true;
 console.log("hello world");
 
 const randomColor = ()=>{
@@ -6,20 +7,29 @@ const randomColor = ()=>{
     for(let i=0 ; i<6 ; i++){
         color+= hex[Math.floor(Math.random()*16)];
     }
+    console.log(color);
     return color;
 }
 
-const changeColor = ()=>{
+const changeColor = ()=>{    
     document.querySelector(".cont").style.backgroundColor = randomColor();
     document.querySelector(".name").innerHTML = randomColor();
 }
 
 let inter;
 document.querySelector(".btn1").addEventListener('click' , ()=>{
-    inter = setInterval(changeColor , 500);
+    document.querySelector(".btn1").disabled = true;
+    document.querySelector(".btn2").disabled = false;
+    if(!inter){
+        inter = setInterval(changeColor , 500);
+    }
+    
 });
 
 
 document.querySelector(".btn2").addEventListener('click' , ()=>{
+    document.querySelector(".btn1").disabled = false;
+    document.querySelector(".btn2").disabled = true;
     clearInterval(inter);
+    inter = null;
 });
